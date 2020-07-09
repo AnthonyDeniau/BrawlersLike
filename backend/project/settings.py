df4ce7fe8190620a76bab0375c44aa5203b44559
Manu <emmanuel.guihur@viacesi.fr>
2020-07-09 14:42:22 +0200

    added apps in installed apps

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -33,7 +33,8 @@ INSTALLED_APPS = [
     'django.contrib.admin', 'django.contrib.auth',
     'django.contrib.contenttypes', 'django.contrib.sessions',
     'django.contrib.messages', 'django.contrib.staticfiles', 'apps.brawler',
-    'graphene_django'
+    'graphene_django',
+    'apps.ability', 'apps.projectil', 'apps.projectilPattern'
 ]
 
 MIDDLEWARE = [
