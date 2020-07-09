c73147cb5009e7cf702ad9248f156e7ab6b5b350
Mickael Bayer <mickael.bayer@gmail.com>
2020-07-09 17:57:47 +0200

    Update brawler

--- backend/project/schema.py
+++ backend/project/schema.py
@@ -4,14 +4,10 @@ from apps.userprofile import schema as userprofile_schema
 from apps.userprofilebrawler import schema as userprofilebrawler_schema
 
 
-class Query(brawler_schema.Query, graphene.ObjectType):
+class Query(brawler_schema.Query, userprofile_schema.Query,
+            userprofilebrawler_schema.Query, graphene.ObjectType):
     pass
 
-class Query(userprofile_schema.Query, graphene.ObjectType):
-    pass
-
-class Query(userprofilebrawler_schema.Query, graphene.ObjectType):
-    pass
 
 class Mutation(brawler_schema.Mutation, graphene.ObjectType):
     pass
