4a47c4f33940c55e921e8802da1f8a3758647eb3
AnthonyBettollo <anthony.bettollo@outlook.fr>
2020-07-09 14:34:34 +0200

    First update model Ability

--- backend/apps/ability/models.py
+++ backend/apps/ability/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Agility(models.Model):
+    brawler = models.ForeignKey(),
+    isSuper = models.BooleanField(),
+    name = models.CharField(max_length=125),
+    description = models.TextField(),
+    image = models.URLField(), #ImageField ? requiert Pillow
+    reloadSpeed = models.DecimalField(),
+    attackSpeed = models.DecimalField(),
+    projectilPattern = models.ForeignKey()
+    def __str__(self):
+        return self.name
