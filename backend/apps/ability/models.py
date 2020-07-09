af1411c92a39f21540d5934700f1bae2d1a361ec
AnthonyBettollo <anthony.bettollo@outlook.fr>
2020-07-09 14:47:35 +0200

    fix Ability and use URLField on Image

--- backend/apps/ability/models.py
+++ backend/apps/ability/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
 # Create your models here.
-class Agility(models.Model):
+class Ability(models.Model):
     brawler = models.ForeignKey(models.Brawler, on_delete=models.PROTECT),
     isSuper = models.BooleanField(),
     name = models.CharField(max_length=125),
     description = models.TextField(),
-    image = models.URLField(), #ImageField ? requiert Pillow
+    image = models.URLField(),
     reloadSpeed = models.DecimalField(),
     attackSpeed = models.DecimalField(),
     projectilPattern = models.ForeignKey(models.projectilPattern, on_delete=models.PROTECT)
