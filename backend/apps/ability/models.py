58d4e9fd827ba338212ab74904391004a64dd87d
AnthonyBettollo <anthony.bettollo@outlook.fr>
2020-07-09 14:38:55 +0200

    finish model Ability

--- backend/apps/ability/models.py
+++ backend/apps/ability/models.py
@@ -1,14 +1,16 @@
 from django.db import models
+from BrawlersLike.backend.apps.brawler import Brawler
+from BrawlersLike.backend.apps.projectilPattern import ProjectilPattern
 
 # Create your models here.
 class Agility(models.Model):
-    brawler = models.ForeignKey(),
+    brawler = models.ForeignKey(Brawler, on_delete=models.PROTECT),
     isSuper = models.BooleanField(),
     name = models.CharField(max_length=125),
     description = models.TextField(),
     image = models.URLField(), #ImageField ? requiert Pillow
     reloadSpeed = models.DecimalField(),
     attackSpeed = models.DecimalField(),
-    projectilPattern = models.ForeignKey()
+    projectilPattern = models.ForeignKey(projectilPattern, on_delete=models.PROTECT)
     def __str__(self):
         return self.name
