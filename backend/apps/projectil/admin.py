2f0b75575bb077fceae9e8b1d202ecb631bbe830
Yoann TERUEL <yoann.teruel@gmail.com>
2020-07-09 14:13:59 +0200

    init projectil

--- backend/apps/projectil/admin.py
+++ backend/apps/projectil/admin.py
@@ -1,5 +1,3 @@
 from django.contrib import admin
-from .models import Projectil
 
 # Register your models here.
-admin.site.register(Projectil)
--- backend/apps/projectil/models.py
+++ backend/apps/projectil/models.py
@@ -1,12 +1,3 @@
 from django.db import models
 
-class Projectil(models.Model):
-    name = models.CharField(max_length=125)
-    sprite = models.ImageField()
-    speed = models.DecimalField()
-    hitboxSize = models.DecimalField()
-    damage = models.IntegerField()
-    range = models.DecimalField()
-
-    def __str__(self):
-        return self.name
+# Create your models here.
