2d74c510144c69eae575abd58802a63f1a437d8e
Manu <emmanuel.guihur@viacesi.fr>
2020-07-09 16:42:31 +0200

    fixed schema

--- backend/project/schema.py
+++ backend/project/schema.py
@@ -1,10 +1,13 @@
 import graphene
 from apps.brawler import schema as brawler_schema
+from apps.ability import schema as ability_schema
 
 
 class Query(brawler_schema.Query, graphene.ObjectType):
     pass
 
+class Query(ability_schema.Query, graphene.ObjectType):
+    pass
 
 class Mutation(graphene.ObjectType):
     pass
