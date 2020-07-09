fa20271d8c8cc7a71210bbcf6b779c1491699331
Manu <emmanuel.guihur@viacesi.fr>
2020-07-09 15:33:01 +0200

    fixed fk import

--- backend/apps/ability/models.py
+++ backend/apps/ability/models.py
@@ -1,15 +1,17 @@
 from django.db import models
+from apps.brawler.models import Brawler
+from apps.projectilPattern.models import ProjectilPattern
 
 # Create your models here.
 class Ability(models.Model):
-    brawler = models.ForeignKey('brawler.Brawler', on_delete=models.PROTECT)
+    brawler = models.ForeignKey(Brawler, on_delete=models.PROTECT)
     isSuper = models.BooleanField()
     name = models.CharField(max_length=125)
     description = models.TextField()
     image = models.URLField()
     reloadSpeed = models.DecimalField(decimal_places=2,max_digits=5)
     attackSpeed = models.DecimalField(decimal_places=2,max_digits=5)
-    projectilPattern = models.ForeignKey('projectilPattern.ProjectilPattern', on_delete=models.PROTECT)
+    projectilPattern = models.ForeignKey(ProjectilPattern, on_delete=models.PROTECT)
     
     def __str__(self):
         return self.name
