dc13c498062aa7aa6d5eb0973435953b14120e6a
AnthonyDeniau <anthony.deniau@gmail.com>
2020-07-09 17:21:00 +0200

    fix multiple Query class issue

--- backend/project/schema.py
+++ backend/project/schema.py
@@ -3,14 +3,12 @@ from apps.brawler import schema as brawler_schema
 from apps.ability import schema as ability_schema
 
 
-class Query(brawler_schema.Query, equipment_schema.Query, userprofile_schema.Query,
+class Query(brawler_schema.Query, userprofile_schema.Query,
             userprofilebrawler_schema.Query, graphene.ObjectType):
     pass
 
-class Query(ability_schema.Query, graphene.ObjectType):
-    pass
 
-class Mutation(brawler_schema.Mutation, equipment_schema.Mutation, graphene.ObjectType):
+class Mutation(brawler_schema.Mutation, graphene.ObjectType):
     pass
 
 
