2dcab153d604110da80fb1fe2d71cec1885089f7
AnthonyBettollo <anthony.bettollo@outlook.fr>
2020-07-09 15:01:36 +0200

    Fix foreignKey Ability

--- backend/apps/ability/models.py
+++ backend/apps/ability/models.py
@@ -1,17 +1,15 @@
 from django.db import models
-from .models import Brawler
-from .models import ProjectilPattern
 
 # Create your models here.
 class Ability(models.Model):
-    brawler = models.ForeignKey(Brawler, on_delete=models.PROTECT),
+    brawler = models.ForeignKey('brawler.Brawler', on_delete=models.PROTECT),
     isSuper = models.BooleanField(),
     name = models.CharField(max_length=125),
     description = models.TextField(),
     image = models.URLField(),
     reloadSpeed = models.DecimalField(),
     attackSpeed = models.DecimalField(),
-    projectilPattern = models.ForeignKey(ProjectilPattern, on_delete=models.PROTECT)
+    projectilPattern = models.ForeignKey('projectilPattern.ProjectilPattern', on_delete=models.PROTECT)
     
     def __str__(self):
         return self.name
