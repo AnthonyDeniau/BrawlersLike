638962fb661811f6ef86b8a06a14f39c69ddf2e1
audrey <nunes.audrey31@gmail.com>
2020-07-09 16:16:24 +0200

    schema user_profile

--- backend/project/schema.py
+++ backend/project/schema.py
@@ -1,10 +1,13 @@
 import graphene
 from apps.brawler import schema as brawler_schema
+from apps.userprofile import schema as userprofile_schema
 
 
 class Query(brawler_schema.Query, graphene.ObjectType):
     pass
 
+class Query(userprofile_schema.Query, graphene.ObjectType):
+    pass
 
 class Mutation(brawler_schema.Mutation, graphene.ObjectType):
     pass
