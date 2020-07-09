ce905ea3eff5ff116203eb677f52677f3274dd97
Manu <emmanuel.guihur@viacesi.fr>
2020-07-09 15:33:01 +0200

    fixed fk import

--- backend/apps/ability/models.py
+++ backend/apps/ability/models.py
@@ -5,12 +5,12 @@ from apps.projectilPattern.models import ProjectilPattern
 # Create your models here.
 class Ability(models.Model):
     brawler = models.ForeignKey(Brawler, on_delete=models.PROTECT)
-    isSuper = models.BooleanField(default=False)
+    isSuper = models.BooleanField()
     name = models.CharField(max_length=125)
-    description = models.TextField(default=' ')
-    image = models.URLField(default=' ')
-    reloadSpeed = models.DecimalField(decimal_places=2,max_digits=5,default =1)
-    attackSpeed = models.DecimalField(decimal_places=2,max_digits=5,default =1)
+    description = models.TextField()
+    image = models.URLField()
+    reloadSpeed = models.DecimalField(decimal_places=2,max_digits=5)
+    attackSpeed = models.DecimalField(decimal_places=2,max_digits=5)
     projectilPattern = models.ForeignKey(ProjectilPattern, on_delete=models.PROTECT)
     
     def __str__(self):
