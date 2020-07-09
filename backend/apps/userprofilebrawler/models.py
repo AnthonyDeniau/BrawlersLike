0b6cf9f7e3cc12723ffaa1f53af97047f0e22e94
audrey <nunes.audrey31@gmail.com>
2020-07-09 15:08:25 +0200

    sans s

--- backend/apps/userprofilebrawler/models.py
+++ backend/apps/userprofilebrawler/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from apps.brawler.models import Brawler
-## from apps.equipments.models import Equipments
-## from apps.abilities.models import Abilities
+## from apps.equipment.models import Equipment
+## from apps.ability.models import Ability
 
 
 # Create your models here.
@@ -10,8 +10,8 @@ class UserProfileBrawler(models.Model):
     brawler = models.ForeignKey(Brawler, on_delete=models.CASCADE)
     level = models.IntegerField()
     power_points = models.IntegerField()
-    ## equipments = models.ManyToManyField(Equipments)
-    ## abilities = models.ManyToManyField(Abilities)
+    ## equipments = models.ManyToManyField(Equipment)
+    ## abilities = models.ManyToManyField(Ability)
 
 
     def __str__(self):
