2d7299d7c313dea9e53c3106bb9ca4126c19c47b
Yoann TERUEL <yoann.teruel@gmail.com>
2020-07-09 17:28:25 +0200

    Fix mutations

--- backend/project/schema.py
+++ backend/project/schema.py
@@ -1,6 +1,7 @@
 import graphene
 from apps.brawler import schema as brawler_schema
 from apps.ability import schema as ability_schema
+from apps.projectil import schema as projectil_schema
 
 
 class Query(brawler_schema.Query, graphene.ObjectType):
@@ -13,4 +14,4 @@ class Mutation(projectil_schema.Mutations, graphene.ObjectType):
     pass
 
 
-schema = graphene.Schema(query=Query, mutation=Mutations)
+schema = graphene.Schema(query=Query, mutation=Mutation)
