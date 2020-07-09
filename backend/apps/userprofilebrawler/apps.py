965c9b5313e8de6bcc70068d88bf0a0ddff3bd5e
Haskimo <33072956+Haskimo@users.noreply.github.com>
2020-07-09 14:19:57 +0200

    start app en vrai

---
+++ backend/apps/userprofilebrawler/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class UserprofilebrawlerConfig(AppConfig):
+    name = 'userprofilebrawler'
--- backend/userbrawler/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class UserbrawlerConfig(AppConfig):
-    name = 'userbrawler'
