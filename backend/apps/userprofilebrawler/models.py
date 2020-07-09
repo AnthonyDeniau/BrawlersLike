144bc4bacea0ec9d68f5dd2a6dc77afaacfd5e82
tesdy14 <tesdy14@hotmail.fr>
2020-07-09 15:06:36 +0200

    relations au singulier

--- backend/apps/userprofilebrawler/models.py
+++ backend/apps/userprofilebrawler/models.py
@@ -1,17 +1,16 @@
 from django.db import models
 from apps.brawler.models import Brawler
-from apps.equipment.models import Equipment
-## from apps.ability.models import Ability
+from apps.equipments.models import Equipment
+from apps.abilities.models import Abilitie
 
 
 # Create your models here.
-
 class UserProfileBrawler(models.Model):
     brawler = models.ForeignKey(Brawler, on_delete=models.CASCADE)
     level = models.IntegerField()
     power_points = models.IntegerField()
     equipments = models.ManyToManyField(Equipment)
-    ## abilities = models.ManyToManyField(Ability)
+    abilities = models.ManyToManyField(Abilitie)
 
 
     def __str__(self):
