02d0a593bd4453a8eabf000ccc9f95d2da97cc27
AnthonyBettollo <anthony.bettollo@outlook.fr>
2020-07-09 14:53:42 +0200

    fix models

--- backend/apps/ability/models.py
+++ backend/apps/ability/models.py
@@ -1,16 +1,17 @@
 from django.db import models
-from .model import Brawler
+from .models import Brawler
+from .models import ProjectilPattern
 
 # Create your models here.
 class Ability(models.Model):
-    brawler = models.ForeignKey(models.Brawler, on_delete=models.PROTECT),
+    brawler = models.ForeignKey(Brawler, on_delete=models.PROTECT),
     isSuper = models.BooleanField(),
     name = models.CharField(max_length=125),
     description = models.TextField(),
     image = models.URLField(),
     reloadSpeed = models.DecimalField(),
     attackSpeed = models.DecimalField(),
-    projectilPattern = models.ForeignKey(models.ProjectilPattern, on_delete=models.PROTECT)
+    projectilPattern = models.ForeignKey(ProjectilPattern, on_delete=models.PROTECT)
     
     def __str__(self):
         return self.name
