5b62229a6868f3635da086c05e1173833a5e14f3
Haskimo <33072956+Haskimo@users.noreply.github.com>
2020-08-28 11:49:32 +0200

    Schema profile_brawler

--- backend/apps/userprofilebrawler/schema.py
+++ backend/apps/userprofilebrawler/schema.py
@@ -9,9 +9,70 @@ class UserProfileBrawlerType(DjangoObjectType):
         model = UserProfileBrawler
 
 
+class CreateUserProfileBrawlerMutation(graphene.Mutation):
+    class Arguments:
+        brawler = graphene.Int()
+        level = graphene.Int()
+        power_points = graphene.Int()
+        equipments = graphene.Int()
+        abilities = graphene.Int()
+
+    ok = graphene.Boolean()
+    userProfileBrawler = graphene.Field(lambda: UserProfileBrawler)
+
+    def mutate(root, info, brawler, level, power_points, equipments, abilities):
+        userProfileBrawler = UserProfileBrawler(
+            brawler=brawler,
+            level=level,
+            power_points=power_points, 
+            equipments=equipments,
+            abilities=abilities
+            )
+        ok = True
+        return CreateUserProfileBrawlerMutation(UserProfileBrawler=userProfileBrawler, ok=ok)
+
+
+class UpdateUserProfileBrawlerMutation(graphene.Mutation):
+    class Arguments:
+        brawler = graphene.String()
+        level = graphene.Int()
+        power_points = graphene.String()
+        equipments = graphene.Int()
+        abilities = graphene.Int()
+
+    ok = graphene.Boolean()
+    userProfileBrawler = graphene.Field(lambda: UserProfileBrawler)
+
+    def mutate(root, info, **kwargs):
+        userProfileBrawler = UserProfileBrawler.objects.get(pk=kwargs['id'])
+        for k, v in kwargs.items():
+            userProfileBrawler.k = v
+        userProfileBrawler.save()
+        ok = True
+        return UpdateUserProfileBrawlerMutation(UserProfileBrawler=userProfileBrawler, ok=ok)
+
+
+class DeleteUserProfileBrawlerMutation(graphene.Mutation):
+    class Arguments:
+        id=graphene.ID()
+
+    ok = graphene.Boolean()
+
+    def mutate(root, info, id):
+        userProfileBrawler = UserProfileBrawler.objects.get(pk=id)
+        userProfileBrawler.delete()
+        ok = True
+        return DeleteUserProfileBrawlerMutation(UserProfileBrawler=userProfileBrawler, ok=ok)
+
+
+class Mutation(graphene.ObjectType):
+    create_user_profile_brawler = CreateUserProfileBrawlerMutation.Field()
+    update_user_profile_brawler = UpdateUserProfileBrawlerMutation.Field()  
+    delete_user_profile_brawler = DeleteUserProfileBrawlerMutation.Field()
+
+
 class Query(graphene.ObjectType):
-    user_profile_brawler = graphene.Field(UserProfileBrawlerType,
-                             id=graphene.Int())
+    user_profile_brawler = graphene.Field(UserProfileBrawlerType, id=graphene.Int())
     user_profile_brawlers = graphene.List(UserProfileBrawlerType)
 
     def resolve_user_profile_brawler(self, context, id=None):
