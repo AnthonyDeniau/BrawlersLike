72a8c02a8152a9a481c4806c4e0c527c7fcbffc2
Mickael Bayer <mickael.bayer@gmail.com>
2020-08-28 13:36:37 +0200

    Schema equipment

--- backend/project/schema.py
+++ backend/project/schema.py
@@ -2,14 +2,15 @@ import graphene
 from apps.brawler import schema as brawler_schema
 from apps.userprofile import schema as userprofile_schema
 from apps.userprofilebrawler import schema as userprofilebrawler_schema
+from apps.equipment import schema as equipment_schema
 
 
-class Query(brawler_schema.Query, userprofile_schema.Query,
+class Query(brawler_schema.Query, equipment_schema.Query, userprofile_schema.Query,
             userprofilebrawler_schema.Query, graphene.ObjectType):
     pass
 
 
-class Mutation(brawler_schema.Mutation, graphene.ObjectType):
+class Mutation(brawler_schema.Mutation, equipment_schema.Mutation, graphene.ObjectType):
     pass
 
 
