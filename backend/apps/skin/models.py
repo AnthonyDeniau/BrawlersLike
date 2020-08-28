ca0c5c72152c83ce9cb1592bf618506124dd2c7c
AnthonyBettollo <anthony.bettollo@outlook.fr>
2020-08-28 15:15:56 +0200

    Fix migrate and model

--- backend/apps/skin/models.py
+++ backend/apps/skin/models.py
@@ -5,10 +5,10 @@ class Skin(models.Model):
     name = models.CharField(max_length=125)
     description = models.TextField()
     avatar = models.URLField()
-    price = models.DecimalField(decimal_places=2,max_digits=5),
-    ThreeDModelFile = models.URLField(),
-    TextureFile = models.URLField(),
-    VoiceLineFile = models.URLField(),
+    price = models.DecimalField(decimal_places=2,max_digits=5)
+    ThreeDModelFile = models.URLField()
+    TextureFile = models.URLField()
+    VoiceLineFile = models.URLField()
     
     def __str__(self):
         return self.name
