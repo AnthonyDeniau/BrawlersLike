f68f48c48fd9da5725ad7f133705d6f648427ba1
Manu <emmanuel.guihur@viacesi.fr>
2020-08-28 15:15:37 +0200

    add skin app

---
+++ backend/apps/skin/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Skin(models.Model):
+    name = models.CharField(max_length=125)
+    description = models.TextField()
+    avatar = models.URLField()
+    price = models.IntegerField()
+    modelFile = models.URLField()
+    textureFile = models.URLField()
+    voiceLineFile = models.URLField()
+    
+    def __str__(self):
+        return self.name
